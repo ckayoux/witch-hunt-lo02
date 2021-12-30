2acd0ceb9c1dafb2dd2b90b158ce4392b952cabc
ckayoux <felix.houdebert@utt.fr>
2021-12-30 13:42:55 +0100

    doc

--- WitchHunt/src/java/fr/sos/witchhunt/model/cards/ExistingRumourCards.java
+++ WitchHunt/src/java/fr/sos/witchhunt/model/cards/ExistingRumourCards.java
@@ -7,11 +7,30 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+/**
+ *<p><b>Singleton containing a set of all existing {@link RumourCard Rumour cards}.</b></p>
+ *<p>Can be instantiated only once, using the {@link #getInstance()} method.</p> 
+ *<p>At instantiation, creates a {@link java.util.Set set} and instantiates all existing Rumour cards. When adding a new Rumour card to the game, 
+ * call its constructor in this class' {@link #ExistingRumourCards() constructor}. Each type of Rumour card must have an unique instance within this class' {@link #existingCardsSet set}.</p>
+ *<p>{@link RumourCard Rumour cards} should only be instantiated by this class, as there must exist no duplicate instances of a same type of Rumour card.</p>
+ * @see RumourCard
+ * @see fr.sos.witchhunt.controller.Tabletop#getAllCardsPile() Tabletop::getAllCardsPile()
+ */
 public final class ExistingRumourCards {
+	/**
+	 * The unique instance of this class. Initialized and obtained using {@link #getInstance()}.
+	 * @see https://refactoringguru.cn/design-patterns/singleton Singleton design pattern
+	 */
 	private static volatile ExistingRumourCards instance = null;
-	
+	/**
+	 * The set of all {@link ExistingRumourCards}. Must never contain duplicate instances of a same type of {@link RumourCard Rumour cards}.
+	 * @see java.util.Set
+	 */
 	private Set <RumourCard> existingCardsSet;
 	
+	/**
+	 * Creates one unique instance of each existing type of {@link RumourCard Rumour card} and adds it to the {@link #existingCardsSet set of existing Rumour cards}.
+	 */
 	private ExistingRumourCards() {
 		existingCardsSet = new HashSet<RumourCard>();
 		existingCardsSet.add(new AngryMob());
@@ -28,6 +47,12 @@ public final class ExistingRumourCards {
 		existingCardsSet.add(new Wart());
 	}
 	
+	/**
+	 * <b>Instantiates an object of this class if it has never been done during the current session.</b>
+	 * @return The unique {@link #instance} of this class.
+	 * @see https://refactoringguru.cn/design-patterns/singleton Singleton design pattern
+	 * @see #instance
+	 */
 	public final static ExistingRumourCards getInstance() {
 		if(ExistingRumourCards.instance==null) {
 			synchronized(ExistingRumourCards.class) {
@@ -44,6 +69,13 @@ public final class ExistingRumourCards {
 		return new ArrayList(existingCardsSet);
 	}
 	
+	/**
+	 * Returns the instance of the unique extension of {@link RumourCard} that is of the given class, contained within the {@link #existingCardsSet set of existing Rumour cards}.
+	 * @param cls The class corresponding to a type of Rumour card. For example, {@link AngryMob AngryMob.class}.
+	 * @return The instance of the unique Rumour card of the given class.
+	 * @see RumourCard
+	 * @see fr.sos.witchhunt.model.players.cpustrategies.CPUStrategy Used by each CPU Players' chosen strategy to associate each existing Rumour Card with a value.
+	 */
 	public static RumourCard getInstanceByClass(Class cls) {
 		Iterator<RumourCard> it = getInstance().existingCardsSet.iterator();
 		RumourCard instance=null;
